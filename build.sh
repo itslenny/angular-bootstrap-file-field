0987da432a07504c3dde901505b8e41aadbcce2e
Lenny Urbanowski <lenny@itslennysfault.com>
2015-02-21 13:55:12 -0800

    gh-pages demo

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#creates minified version and source map
-#requires uglifyjs -- npm install uglifyjs -g
-
-uglifyjs ./src/angular-bootstrap-file-field.js --source-map ./dist/angular-bootstrap-file-field.min.js.map --output ./dist/angular-bootstrap-file-field.min.js
