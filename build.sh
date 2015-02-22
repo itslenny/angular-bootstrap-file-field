33381601c56bdcd57bacd6be2bd2f5ec760f98bd
Lenny Urbanowski <lenny@itslennysfault.com>
2015-02-21 17:09:41 -0800

    fixed build script

--- build.sh
+++ build.sh
@@ -3,4 +3,6 @@
 #creates minified version and source map
 #requires uglifyjs -- npm install uglifyjs -g
 
-uglifyjs ./src/angular-bootstrap-file-field.js --comments --source-map ./dist/angular-bootstrap-file-field.min.js.map --source-map-url angular-bootstrap-file-field.min.js.map --output ./dist/angular-bootstrap-file-field.min.js
+PROJECT_NAME=angular-bootstrap-file-field
+
+uglifyjs ./src/${PROJECT_NAME}.js --comments --source-map ./dist/${PROJECT_NAME}.min.js.map --source-map-url ${PROJECT_NAME}.min.js.map --output ./dist/${PROJECT_NAME}.min.js
